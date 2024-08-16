43e0a332363be6ec4b325da820967f4972c43885
mmhossain <mosharaf.hossain221@gmail.com>
2024-08-15 17:01:31 -0700

    Add new row to the spreadsheet data source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { SheetDescriptor, SpreadsheetMainMenuItem } from '@progress/kendo-angular-spreadsheet';
+import { Component, NgZone } from '@angular/core';
+import { SheetDescriptor, SheetRow, SpreadsheetMainMenuItem } from '@progress/kendo-angular-spreadsheet';
 import { sheets } from './sheets';
 
 @Component({
@@ -13,4 +13,34 @@ export class AppComponent {
         { id: 'home' },
         { id: 'insert' }
     ];
+
+    constructor(private zone: NgZone){}
+
+    public AddRow(): void {
+      let rows = this.sheets[0].rows ?? [];
+        
+      let index = rows?.findIndex(r => r.index != null) ?? 1;
+      let existingRow = rows[index - 1];
+      let newCells = existingRow.cells?.map(cell => ({
+        ...cell
+      }));
+
+      let newRow = {...existingRow, cells: newCells };
+      if (newRow.cells && newRow.cells.length > 0)
+        newRow.cells[0].value = 123;
+      
+      rows.splice(index, 0, newRow);
+      
+      this.zone.run(() => {
+        this.sheets = this.sheets.map(sh => {
+          if (sh.rows != null) {
+            return ({...sh, rows: [...rows] as SheetRow[]})
+          }
+          else
+            return ({...sh})
+        }) as SheetDescriptor[];
+
+        console.log(this.sheets[0].rows?.length);
+      });
+    }
 }
